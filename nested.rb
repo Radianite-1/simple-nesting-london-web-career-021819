81f1afdc9eb50ddc80c657c8661d4cae0702d329
Luke Ojukwu <luke.ojukwu@gmail.com>
2019-02-05 17:51:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -96,7 +96,6 @@ def adding_matz
 end
 
 def changing_alan
-  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -113,11 +112,12 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-      
-      programmer_hash[:alan_kay][:known_for] = "GUI"
-      
-      programmer_hash[:alan_kay][:known_for]
-
+     alans_new_info = "GUI"
+     
+     
+     programmer_hash[:alan_kay][:known_for]
+     
+     
 end
 
 def adding_to_dennis
