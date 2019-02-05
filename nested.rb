2ea21f29ca4a25826b3edea578f6f42faf391f0d
Luke Ojukwu <luke.ojukwu@gmail.com>
2019-02-05 17:53:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,10 +115,10 @@ def changing_alan
      alans_new_info = "GUI"
      
      
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash[:alan_kay][:known_for] => "GUI"
      
      
-     return alans_new_info
+   
      
 end
 
